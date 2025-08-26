d70f93ca374e31f4f107d39795abcbc2773da570
SPIDEY OFFICIAL <gaminghatyar777@gmail.com>
2025-08-26 17:06:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import eventlet
+eventlet.monkey_patch()
 import os
 import secrets
 import string
@@ -15,8 +17,6 @@ from flask import Flask, render_template, request, redirect, url_for, session, f
 from flask_socketio import SocketIO, emit, join_room, leave_room, rooms
 from email_validator import validate_email, EmailNotValidError
 from dotenv import load_dotenv
-import eventlet
-eventlet.monkey_patch()
 
 # Load environment variables
 load_dotenv()
